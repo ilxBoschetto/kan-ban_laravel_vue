4e6738ea4f76982926ca5dc06b73037dd4985eda
ilxBoschetto <matbos2003@gmail.com>
2025-04-27 17:09:57 +0200

    Added couple of themes

--- resources/js/components/user/Theme.vue
+++ resources/js/components/user/Theme.vue
@@ -1,29 +1,41 @@
 <template>
-    <button @click="toggleTheme">
-        Cambia tema
-    </button>
+    <div class="row gap-2">
+        <div v-for="theme in themes" :key="theme.name" class="col-md-3" @click="setTheme(theme.name)">
+            <div class="theme-name"
+                :class="{ active: currentTheme === theme.name }">
+                {{ theme.name.toUpperCase() }}
+            </div>
+            <div class="theme-option" :style="{ backgroundColor: theme.color }"
+                :class="{ active: currentTheme === theme.name }">
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 
-const theme = ref('light')
+const currentTheme = ref('light');
+const themes = [
+    { name: 'light', color: '#ffffff' },
+    { name: 'dark', color: '#2c3e50' },
+    { name: 'lavanda', color: '#726a85' },
+    { name: 'blue sky', color: '#aeb7c4' },
+    { name: 'red desert', color: '#b96f57' },
+    { name: 'green olive', color: '#7b8b6f' },
+]
 
-const applyTheme = () => {
-    document.documentElement.setAttribute('data-theme', theme.value)
-    localStorage.setItem('theme', theme.value)
-}
-
-const toggleTheme = () => {
-    theme.value = theme.value === 'light' ? 'dark' : 'light'
-    applyTheme()
+function setTheme(themeName) {
+    currentTheme.value = themeName;
+    document.documentElement.setAttribute('data-theme', currentTheme.value);
+    localStorage.setItem('theme', currentTheme.value);
 }
 
 onMounted(() => {
-    // Recupera il tema salvato o imposta "light"
-    theme.value = localStorage.getItem('theme') || 'light'
-    applyTheme()
-})
+    let theme = localStorage.getItem('theme');
+    currentTheme.value = theme ?? 'light';
+    setTheme(currentTheme.value);
+});
 </script>
 
 <style>
@@ -45,5 +57,29 @@ button {
     cursor: pointer;
     border-radius: 6px;
     transition: background-color 0.2s ease;
+}
+
+.theme-switcher {
+    display: flex;
+    gap: 1rem;
+}
+
+.theme-option {
+    border-radius: 0.5rem;
+    cursor: pointer;
+    height: 5rem;
+    border: 0.1rem solid silver;
+    transition: border-color 0.3s, transform 0.3s;
+}
+
+.theme-option:hover {
+    transform: scale(1.01);
+}
+
+.theme-option.active {
+    border: 0.2rem solid var(--custom-success, #e67e22);
+}
+.theme-name.active{
+    font-weight: 800;
 }
 </style>
